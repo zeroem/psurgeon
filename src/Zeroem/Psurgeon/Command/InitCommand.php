d788d8f6d41da4f35dfa1a9b36c455185f22cf16
Darrell Hamilton <darrell.noice@gmail.com>
2012-08-08 23:02:36 -0700

    optionally add .psurgeon.sqlite to .gitignore

--- src/Zeroem/Psurgeon/Command/InitCommand.php
+++ src/Zeroem/Psurgeon/Command/InitCommand.php
@@ -11,13 +11,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class InitCommand extends Command
 {
-    private $sqlite;
     private $init;
-    
-    public function __construct($sqlite,$init) {
-        parent::__construct();
+    private $sqlite;
 
-        $this->sqlite = $sqlite;
+    public function __construct($init) {
+        parent::__construct();
+        $this->sqlite = 'sqlite:' . realpath('.') . '/.psurgeon.sqlite';
         $this->init = $init;
     }
 
@@ -27,8 +26,71 @@ class InitCommand extends Command
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
+        $dialog = $this->getHelperSet()->get('dialog');
+
         $pdo = new \PDO($this->sqlite);
         $result = $pdo->query($this->init);
+
+        if ($input->isInteractive()) {
+            $ignoreFile = realpath('.gitignore');
+
+            if (false === $ignoreFile) {
+                $ignoreFile = realpath('.') . '/.gitignore';
+            }
+
+            if (!$this->hasPsurgeonIndexIgnore($ignoreFile)) {
+                $question = 'Would you like the <info>psurgeon index</info> database added to your <info>.gitignore</info> [<comment>yes</comment>]?';
+
+                if ($dialog->askConfirmation($output, $question, true)) {
+                    $this->addPsurgeonIndexIgnore($ignoreFile);
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks the local .gitignore file for the Psurgeon index database.
+     *
+     * Copied from the Composer Init Command
+     *
+     * @param string $ignoreFile
+     * @param string $db
+     *
+     * @return Boolean
+     */
+    private function hasPsurgeonIndexIgnore($ignoreFile, $db = '.psurgeon.sqlite')
+    {
+        if (!file_exists($ignoreFile)) {
+            return false;
+        }
+
+        $pattern = sprintf(
+            '~^/?%s(/|/\*)?$~',
+            preg_quote($db, '~')
+        );
+
+        $lines = file($ignoreFile, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $line) {
+            if (preg_match($pattern, $line)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function addPsurgeonIndexIgnore($ignoreFile, $db = '.psurgeon.sqlite')
+    {
+        $contents = "";
+        if (file_exists($ignoreFile)) {
+            $contents = file_get_contents($ignoreFile);
+
+            if ("\n" !== substr($contents, 0, -1)) {
+                $contents .= "\n";
+            }
+        }
+
+        file_put_contents($ignoreFile, $contents . $db. "\n");
     }
 }
 
