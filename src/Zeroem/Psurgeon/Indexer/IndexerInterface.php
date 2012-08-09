88f1def35a629ae85da08d729eb74ab33f0d056f
Darrell Hamilton <darrell.noice@gmail.com>
2012-08-09 01:26:23 -0700

    object based indexing interface

--- src/Zeroem/Psurgeon/Indexer/IndexerInterface.php
+++ src/Zeroem/Psurgeon/Indexer/IndexerInterface.php
@@ -2,8 +2,28 @@
 
 namespace Zeroem\Psurgeon\Indexer;
 
+use Zeroem\Psurgeon\TokenNode;
+
 interface IndexerInterface
 {
-  function registerClass($className, $fileName, $namespace=null);
-  function registerMethod($classId, $methodName)
+  /**
+   * Determine whether or not the indexer can handle this node
+   * in the give context
+   *
+   * @param ContextChain $context the current context
+   * @param TokenNode $node the node to index
+   *
+   * @return boolean
+   */
+  function canIndex(ContextChain $context, TokenNode $node);
+
+  /**
+   * Generate an Index Entity based on the give node and context
+   *
+   * @param ContextChain $context the current context
+   * @param TokenNode $node the node to index
+   *
+   * @return Index
+   */
+  function index(ContextChain $context, TokenNode $node);
 }
