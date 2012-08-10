2ce8b86a2bc13c7d666e579544d0975dd31c6deb
Darrell Hamilton <darrell.noice@gmail.com>
2012-08-10 00:07:37 -0700

    finished basic indexer algorithm, added block handling

--- src/Zeroem/Psurgeon/Indexer/Indexer.php
+++ src/Zeroem/Psurgeon/Indexer/Indexer.php
@@ -3,6 +3,7 @@
 namespace Zeroem\Psurgeon\Indexer;
 
 use Zeroem\Psurgeon\Tokenizer;
+use Zeroem\Psurgeon\Indexer\Entity\FileIndex;
 
 class Indexer
 {
@@ -13,19 +14,39 @@ class Indexer
 
   public function index($filename) {
     $context = new ContextChain();
-    $context->push(new Context($filename));
+    $blocks = array();
 
     $tokenizer = new Tokenizer();
     $indexes = array();
+    $nextBlockAction = null;
 
-    $tokenChain = $tokenizer->tokenize(file_get_contents($filename));
-    
+    $content = file_get_contents($filename);
+    $context->push(new FileIndex($filename, md5($content)));
+
+    $tokenChain = $tokenizer->tokenize($content);
     $node = $tokenChain->getHead();
-    
+
     while(null !== $node) {
       foreach($this->indexers as $indexer) {
         if($indexer->canIndex($contextChain, $node)) {
-          $indexes[] = $reactor->index($contextChain, $node);
+          $index = $indexer->index($contextChain, $node);
+
+          $indexes[] = $index;
+          if($indexer->isContextual()) {
+            $this->context->push($index);
+            $nextBlockAction = array($this->context, 'pop');
+          }
+        }
+      }
+
+      if('{' === $node->getToken()->getText() ) {
+        array_push($blocks, $nextBlockAction);
+        $nextBlockAction = null;
+      } else if( '}' === $node->getToken()->getText() ) {
+        $action = array_pop($blocks);
+
+        if(is_callable($action)) {
+          call_user_func($action);
         }
       }
     }
@@ -34,6 +55,6 @@ class Indexer
   }
 
   public function addIndexer($indexer) {
-    $this->reactors[] = $something;
+    $this->indexers[] = $indexer;
   }
 }
